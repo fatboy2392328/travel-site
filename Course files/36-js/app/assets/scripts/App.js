11b6e46879b9ead3d08e7002b6d6c667a43aca17
Perry <fatboy2392328@gmail.com>
2022-11-19 17:47:36 -0500

    completed js

---
+++ Course files/36-js/app/assets/scripts/App.js
@@ -0,0 +1,22 @@
+import '../styles/styles.css'
+
+if (module.hot) {
+  module.hot.accept()
+}
+
+/* Lesson example code below this line */
+class Person {
+  constructor(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function () {
+      console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    };
+  }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-const path = require('path')
+/** @format */
+
+const path = require('path');
 
 const postCSSPlugins = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer')
-]
+	require('postcss-import'),
+	require('postcss-mixins'),
+	require('postcss-simple-vars'),
+	require('postcss-nested'),
+	require('postcss-hexrgba'),
+	require('autoprefixer'),
+];
 
 module.exports = {
 	entry: './app/assets/scripts/App.js',
@@ -34,7 +37,7 @@ module.exports = {
 				test: /\.css$/i,
 				use: [
 					'style-loader',
-          { loader: 'css-loader', options: { url: false, } },
+					{ loader: 'css-loader', options: { url: false } },
 					{ loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } },
 				],
 			},
